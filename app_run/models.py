41c03d83be26adfa836e6f4879fc9f542a7c7e27
Imabirdmf <vash.stampide@gmail.com>
2025-07-28 22:39:21 +0100

    Created Run model and api endpoint for CRUD ops

--- app_run/models.py
+++ app_run/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Run(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    athlete = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    comment = models.CharField(max_length=200)
